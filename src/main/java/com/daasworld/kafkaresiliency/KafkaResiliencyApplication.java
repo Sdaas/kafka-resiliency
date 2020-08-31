5748040de2b395a8c07e65f0460ace45420b6c2f
Soumendra Daas <soumendra.daas@gmail.com>
2020-08-31 13:31:07 +0530

    first commit - simple Kafka consumer with no resiliency

---
+++ src/main/java/com/daasworld/kafkaresiliency/KafkaResiliencyApplication.java
@@ -0,0 +1,13 @@
+package com.daasworld.kafkaresiliency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaResiliencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaResiliencyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/daasworld/kafkaresiliency/MyKafkaListener.java
@@ -0,0 +1,27 @@
+package com.daasworld.kafkaresiliency;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.support.GenericMessage;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class MyKafkaListener {
+
+    @KafkaListener(topics = "test-topic") // errorHandler = "customErrorHandler"
+    public void listen(@Payload String value,
+        @Header(name = KafkaHeaders.RECEIVED_MESSAGE_KEY, required = false) String key,
+        @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition,
+        @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+        @Header(KafkaHeaders.RECEIVED_TIMESTAMP) long ts,
+        @Header(KafkaHeaders.OFFSET) long offset
+    ) {
+        log.info("Reading topic={} partition={} offset={}, key={}, value={}", topic, partition, offset, key, value );
+    }
+
+
+}
